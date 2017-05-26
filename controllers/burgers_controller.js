30e1bb41da8be4b4a40fc6dee372458a485f330d
benjinchang <benjinchang@gmail.com>
2017-05-26 03:07:51 -0700

    updated logic and routing

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,7 @@ var db = require('../models');
 var express = require('express');
 var router  = express.Router();
 
+// Displays all burgers available from the mySQL db
 router.get('/', function(req,res) {
   db.Burger.findAll({
   }).then(function(burger_data){
@@ -11,23 +12,40 @@ router.get('/', function(req,res) {
   })
 })
 
-router.post('/update', function(req,res){
-  console.log(req.body);
-  db.Burger.update(req.body,
+// Devours burger by updating the devoured boolean to true in the mySQL db
+router.post('/burger/:id', function(req,res){
+  console.log(req.params.id);
+  db.Burger.update(
+    {
+      devoured: true,
+    },
     {
       where: {
-        id: req.body.id
+        id: req.params.id
     }
   }).then(function() {
     res.redirect('/');
   });
-
 });
 
-router.post('/create', function(req,res){
-  burger.create(req.body.burger_name, function(result){
+// Add a new burger to the mySQL db
+router.post('/add', function(req,res) {
+  console.log(req.body.burger_name + " burger added to database!");
+  db.Burger.create({
+    burger_name: req.body.burger_name,
+    devoured: 0
+  }).then(function(){
     res.redirect('/');
-  })
-})
+  });
+});
+
+// Deletes all burgers in the mySQL db
+router.post('/delete', function(req,res) {
+  db.Burger.destroy({
+    where: {}
+  }).then(function(){
+    res.redirect('/');
+  });
+});
 
 module.exports = router;
